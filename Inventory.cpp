a2280f36f6b9dad4634a056c7144b904956724d9
ks731 <kevinsanchessalazar@gmail.com>
2025-02-24 00:39:47 -0500

    discard equip test

--- Inventory.cpp
+++ Inventory.cpp
@@ -23,13 +23,17 @@ Item* Inventory::getEquipped() const{
 
 //Updates `equipped` to the specified item 
 void Inventory::equip(Item* itemToEquip){
-    equipped_ = itemToEquip;
-}
+    std::cout << "Equipping item: " << itemToEquip->name_ << "\n";
 
+        equipped_ = itemToEquip;
+
+}
 
 //Discard Equipped Item
 void Inventory::discardEquipped(){
     if(equipped_ != nullptr){
+std::cout << "Discarding item: " << equipped_->name_ << "\n";
+
         delete equipped_;
         equipped_ = nullptr;
     }
